bba00051ffd191d2c3c572efc3fe7b8fadedb16a
sabareeswar senthilvel <112149224+Sabari202@users.noreply.github.com>
2025-03-18 15:19:40 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -35,7 +35,7 @@ window.onscroll = ()=>{
 // TYPED SCRIPT HOME
 
 var typed =  new Typed(".auto-type",{
-    strings: ["Data Analyst","Data Engineer","Software Engineer","Full-Stack Developer"],
+    strings: ["Software Engineer","Full-Stack Developer","Data Engineer","Data Analyst"],
     typeSpeed: 150,
     backSpeed: 150,
     loop: true
